671373aca659fa190f39f0f4267fda05bdbc0b67
latte <166247241+ribbonboyy@users.noreply.github.com>
2024-11-24 18:50:31 +0100

    Create 1aaa readme.js

---
+++ 1aaa readme.js
@@ -0,0 +1,7 @@
+// hi. 
+//ive made this as a project for fun. 
+///all scripts, html,css, js are written by me
+//i was kinda depressed and need a fun thing to do.
+//i added some explanation to what the scripts do in some of them. 
+//sorry if something doesnt work
+// -4thpatriot on discord
