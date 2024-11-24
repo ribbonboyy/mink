4c9daca6e4dd6010702403f687f2b6aa1c0316cb
latte <166247241+ribbonboyy@users.noreply.github.com>
2024-11-24 18:52:53 +0100

    Update 1aaa readme.js

--- 1aaa readme.js
+++ 1aaa readme.js
@@ -4,4 +4,5 @@
 //i was kinda depressed and need a fun thing to do.
 //i added some explanation to what the scripts do in some of them. 
 //sorry if something doesnt work
+//for curious minds; the file name is 1aaa readme, becasue 1aaa ensures it will always be on top
 // -4thpatriot on discord
