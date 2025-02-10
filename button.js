53a24cd67315fcead14161caf68bb649ba9a76c1
latte <166247241+ribbonboyy@users.noreply.github.com>
2025-02-10 15:22:14 +0100

    Update button.js

--- button.js
+++ button.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const warningPopup = document.getElementById("warning-popup");
-    const agreeBtn = document.getElementById("agree-btn");
-    const mainContent = document.querySelector(".container");
 
-    agreeBtn.addEventListener("click", function () {
-        warningPopup.style.display = "none";  // Hide the popup
-        mainContent.style.display = "block";  // Show the main content
-    });
-});
